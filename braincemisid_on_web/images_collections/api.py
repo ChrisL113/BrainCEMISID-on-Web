89a857141c9e17c4b00c9e9ec11480e095eaeff8
soulblckdstroyer <galileo.20@hotmail.com>
2020-01-08 20:39:45 -0400

    pagination in image

--- braincemisid_on_web/images_collections/api.py
+++ braincemisid_on_web/images_collections/api.py
@@ -3,14 +3,19 @@ from .serializers import ImagesFromNeuronSerializer
 from rest_framework.response import Response
 from django.contrib.auth.models import User
 from images_collections.models import ImagesFromNeuron
+from rest_framework.pagination import PageNumberPagination
 
-
+class StandardResultsSetPagination(PageNumberPagination):
+    page_size = 10
+    page_size_query_param = 'page_size'
+    max_page_size = 100
 
 class UserCollectionViewSet(viewsets.ModelViewSet):
     permission_classes = [
         permissions.AllowAny#IsAuthenticated,
     ]
     serializer_class = ImagesFromNeuronSerializer
+    pagination_class = StandardResultsSetPagination
 
     def get_queryset(self):
         return self.request.user.images_collection.all()
@@ -28,6 +33,7 @@ class AllCollectionsViewSet(viewsets.ModelViewSet):
     ]
 
     serializer_class= ImagesFromNeuronSerializer
+    pagination_class = StandardResultsSetPagination
 
     def perform_create(self, serializer):
         serializer.save(owner=self.request.user)
