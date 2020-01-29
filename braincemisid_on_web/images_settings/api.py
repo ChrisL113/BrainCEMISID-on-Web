d2f39e55b15a916f0f89b75812643f8dcf23de12
KristoLopez <galileo.20@hotmail.com>
2020-01-29 06:40:48 -0400

    cleaning

--- braincemisid_on_web/images_settings/api.py
+++ braincemisid_on_web/images_settings/api.py
@@ -13,23 +13,17 @@ class ImageSettingsSetViewSet(viewsets.ModelViewSet):
 
 
     def create(self, request):
-        #request.user.images_collection.filter()
-        #print(request.data)
+        
         if request.user.imageSettingsFromOwner.filter(image=request.data['image'],brain__id=request.data['brain']):
-            #aux1=request.user.brain.objects#.imageSettingsFromBrain
             return Response({'message':'image_settings is already saved for this image in this project'})   
+        
         if request.user.brain.filter(pk=request.data['brain']):
             serializer = self.serializer_class(data=request.data)
             serializer.is_valid(raise_exception=True)
             serializer.save(owner=request.user)
             return Response(serializer.data, status=status.HTTP_200_OK)
-            #  aux=ImageSettings(r_tolerance=request.data['r_tolerance'],g_tolerance=request.data['g_tolerance'],b_tolerance=request.data['b_tolerance'],owner=request.user)
-            # aux.brain_id=request.data['brain']
-            # aux.image_id=request.data['image']
-            # aux.save()
-            # return Response({'message':'done'})
         return Response(status=status.HTTP_400_BAD_REQUEST)
-        #serializer.save(owner=self.request.user)
+
     def update(self, request,pk):
         id_image_s=pk
         image_to_update=request.user.imageSettingsFromOwner.get(pk=pk)
